90c73d8e1ca0eb7a45facda927df4c095ab519a2
Erick <126675963+Erick-BFS@users.noreply.github.com>
2023-06-23 16:42:04 -0300

    .

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ for(let contador = 0; contador <= 10; contador++) {
     let resultado = tabuada * contador;
     console.log(tabuada + ' x ' + contador + ' = ' + resultado)
 }
+
+// do
+console.log('do while')
+
+let contador3 = 0;
+do {
+    let resultado = tabuada * contador3;
+    console.log(tabuada + ' x ' + contador3 + ' = ' + resultado)
+    contador3++
+} while(contador3 <= 10);
